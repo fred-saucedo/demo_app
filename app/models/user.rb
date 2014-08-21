800422e927f527477ea7b289cc1c659f1aacc525
Fred Saucedo <alfred3863@gmail.com>
2014-08-20 19:47:48 -0700

    Updating Site

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+
+  #a user should have many microposts
+
+  has_many :micropost
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820232509) do
+ActiveRecord::Schema.define(version: 20140821023435) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.string   "string"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
